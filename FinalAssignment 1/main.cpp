39597e19196c0b98137c62ae3d1314dcc4291629
Jasmine Jans <jasminejans@Jasmines-MacBook-Pro-2.local>
2014-01-21 17:45:11 -0800

    Initial Commit

---
+++ FinalAssignment 1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FinalAssignment 1
+//
+//  Created by Jasmine Jans on 1/21/14.
+//  Copyright (c) 2014 Jasmine Jans. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
